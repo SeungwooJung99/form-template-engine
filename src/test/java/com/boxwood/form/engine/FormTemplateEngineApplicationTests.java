af2fca79a2f6bd306e8b7f53bba3431f2f97e22f
glenn <glenn@ecoletree.com>
2025-08-07 11:02:12 +0900

    Init commit

---
+++ src/test/java/com/boxwood/form/engine/FormTemplateEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boxwood.form.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormTemplateEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
